17a41dbda1146c201756d1c503472d369156d88f
Suyash Mankar <suyashmankar@gmail.com>
2022-12-16 02:22:09 +0530

    setup react google oauth and create login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Messenger from "./components/Messenger";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-  return <div className="App">working</div>;
+  const clientId =
+    "476548601525-qe24tu10lv6j6elkegh604jhh258vk0n.apps.googleusercontent.com";
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <Messenger />
+    </GoogleOAuthProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Messenger.js
@@ -0,0 +1,25 @@
+import React from "react";
+import LoginDialog from "./account/LoginDialog";
+import { AppBar, Toolbar, styled, Box } from "@mui/material";
+
+const Component = styled(Box)`
+  height: 100vh;
+  background: #dcdcdc;
+`;
+
+const Header = styled(AppBar)`
+  height: 220px;
+  background-color: #00bfa5;
+  box-shadow: none;
+`;
+
+export default function Messenger() {
+  return (
+    <Component>
+      <Header>
+        <Toolbar></Toolbar>
+      </Header>
+      <LoginDialog />
+    </Component>
+  );
+}
